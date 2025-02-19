2d87b603700f0787d8e40d446ba17e1be104b65d
sultan12-mohd <sm620063@gmail.com>
2025-02-19 07:55:48 +0530

    updation for index.html

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// const buttons = document.querySelectorAll('.button')
-// const body = document.querySelector('body')
-// buttons.forEach(function(button){
-//  button.addEventListener("click",function(e){
-//   if(e.target.id==='grey'){
-//     body.style.backgroundColor=e.target.id;
-//   }
-//   if(e.target.id==='lightgreen'){
-//     body.style.backgroundColor=e.target.id;
-//   }
-//   if(e.target.id==='lightblue'){
-//     body.style.backgroundColor=e.target.id;
-//   }
-//   if(e.target.id==='pink'){
-//     body.style.backgroundColor=e.target.id;
-//   }
-//  })
-// })
 const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 buttons.forEach(function(button){
